63b4775d34f934568c35d8b5c688807c4e60bfd9
Łukasz Kalicki <lukasz.kalicki@billennium.com>
2018-08-14 08:00:09 +0200

    review changes in compile

--- tests/compile.js
+++ tests/compile.js
@@ -1,4 +1,12 @@
-var solc = require('solc')
+var solc = require('solc');
+var fs = require('fs');
+
+var file = "";
+
+fs.readFile('./contracts/test.sol','utf8',function(err,contents) {
+    file.concat(contents)
+});
+console.log(file)
 var input = 'pragma solidity ^0.4.0;contract Test {uint storedData;function set(uint x) public {if (x == 0){x = 1;} else{storedData = x;}}}'
 
 var output = solc.compile(input, 1)
